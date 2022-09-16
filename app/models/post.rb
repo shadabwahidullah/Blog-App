7bf29eabebe2a34cc2a328db2397e64895e98f92
shadabwahidullah <shadabwahidullah@gmail.com>
2022-09-16 19:41:19 +0430

    Add validation for post

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,10 @@ class Post < ApplicationRecord
   has_many :likes
   has_many :comments
 
+  validates :title, presence: true, length: {in: 1..250}
+  validates :comments_counter, numericality: {integer: true, greater_than_or_equal_to: 0}
+  validates :likes_counter, numericality: {integer:true, greater_than_or_equal_to: 0}
+
   def update_post_counter
     author.increment!(:posts_counter)
   end
