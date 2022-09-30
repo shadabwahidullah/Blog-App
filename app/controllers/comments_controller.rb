61b72e242595f971a1ec33761b98827e8aee7ab7
shadabwahidullah <shadabwahidullah@gmail.com>
2022-09-30 22:56:40 +0430

    Add form to add comment

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,25 @@
+class CommentsController < ApplicationController
+
+  def new
+    @comment = Comment.new
+    @author = current_user
+    @post = Post.find(params[:post_id])
+  end
+
+  def create 
+    @comment = Comment.new(params.require(:comment).permit(:text))
+    @comment.author_id = current_user.id
+    @comment.post_id = params[:post_id]
+    @post = Post.find(params[:post_id])
+    respond_to do |format|
+      format.html do
+        if(@comment.save)
+          flash[:success] = 'Comment saved'
+          redirect_to user_post_url(current_user, @post)
+        else
+          flash.now[:error] = "there was an error while saving the comment"
+        end
+      end
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,8 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :new, :create, :show]
   end
+  resources :posts do
+    resources :comments, only: [:new, :create]
+  end
 
 end
