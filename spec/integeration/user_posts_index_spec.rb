1d4ea5b9ee918d8a90c10d0341cd46fe0cb28db8
shadabwahidullah <shadabwahidullah@gmail.com>
2022-10-05 18:29:55 +0430

    Add multiple tests for posts index page

--- spec/integeration/user_posts_index_spec.rb
+++ spec/integeration/user_posts_index_spec.rb
@@ -7,7 +7,11 @@ RSpec.describe 'Users index page', js: true , type: :system do
       @post1 = Post.create(title: 'Rspec test 1', text: 'rspec test for post', author: @user)
       @post2 = Post.create(title: 'Rspec test 2', text: 'rspec test for post', author: @user)
       @post3 = Post.create(title: 'Rspec test 3', text: 'this is the body of rspec test for post', author: @user)
-      # @comment1 = Comment.create(author: @user, post: )
+      @comment1 = Comment.create(author: @user, post: @post1, text: 'this is the first comment' )
+      @comment2 = Comment.create(author: @user, post: @post1, text: 'this is the second comment' )
+      @comment3 = Comment.create(author: @user, post: @post1, text: 'this is the third comment' )
+      @like1 = Like.create(author: @user, post: @post1)
+      @like2 = Like.create(author: @user, post: @post1)
     end
     it 'loads the user profile picture' do
       visit user_posts_path(@user)
@@ -32,6 +36,23 @@ RSpec.describe 'Users index page', js: true , type: :system do
     it 'I should see a post body' do
       visit user_posts_path(@user)
       expect(page).to have_content('this is the body of')
+    end
+    it 'I should see the first comment for post1' do
+      visit user_posts_path(@user)
+      expect(page).to have_content('this is the first comment')
+    end
+    it 'I should see the number of comments' do
+      visit user_posts_path(@user)
+      expect(page).to have_content('Comments: 3')
+    end
+    it 'Should show how may likes a post have' do
+      visit user_posts_path(@user)
+      expect(page).to have_content('Likes: 2')
+    end
+    it 'When I click on a post, it redirects me to that posts show page' do
+      visit user_posts_path(@user)
+      click_link("post:#{@post2.id}")
+      expect(page).to have_current_path(user_post_path(@user, @post))
     end
   end
 end
